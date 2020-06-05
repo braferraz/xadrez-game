3c5d757b4c18912705d2e6f7c028d1ba875d16d6
Braian Ferraz <braianfs2009@gmail.com>
2020-06-05 10:09:40 -0300

    Cavalo

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,6 +9,7 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.Bishop;
 import chess.pieces.King;
+import chess.pieces.Knight;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -194,8 +195,10 @@ public class ChessMatch {
         placeNewPiece('h', 7, new Rook(board, Color.WHITE));
         placeNewPiece('d', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 6, new Knight(board, Color.WHITE));
 
         placeNewPiece('b', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 5, new Rook(board, Color.BLACK));
         placeNewPiece('a', 8, new King(board, Color.BLACK));
 	}
 }
